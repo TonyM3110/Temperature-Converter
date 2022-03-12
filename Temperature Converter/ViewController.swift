ccc35887e78bd6041ba77f05092d4796085741b2
Tony Cherevko <anton.ch.tr@gmail.com>
2022-03-12 12:07:21 +0700

    Temperature Converter (from ºF to ºC)

--- Temperature Converter/ViewController.swift
+++ Temperature Converter/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+  
+    @IBOutlet weak var celsiusLable: UILabel!
+    @IBOutlet weak var fahrenheitLable: UILabel!
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.maximumValue = 100
+            slider.minimumValue = 0
+            slider.value = 0
+        }
     }
-
-
+   
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        let temeratureCelsius = Int(round(sender.value))
+        celsiusLable.text = "\(temeratureCelsius)ºC"
+        let fahrenheitTemereture = Int(round((sender.value * 9 / 5) + 32))
+        fahrenheitLable.text = "\(fahrenheitTemereture)ºF"
+    }
+    
 }
 
